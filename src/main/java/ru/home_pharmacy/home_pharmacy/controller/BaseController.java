5f71e2e52fa9edf5a8495ac144e38a294f2eef64
AlikaMinakova <minackowa.ali@yandex.ru>
2025-09-07 09:21:02 +0300

    create init project

---
+++ src/main/java/ru/home_pharmacy/home_pharmacy/controller/BaseController.java
@@ -0,0 +1,26 @@
+package ru.home_pharmacy.home_pharmacy.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/")
+public class BaseController {
+    @GetMapping("/medicine")
+    public String create_medicine() {
+        return "medicine";
+    }
+
+    @GetMapping("/add-medicine")
+    public String add_medicine() {
+        return "add_medicine";
+    }
+
+    @GetMapping("/diseases")
+    public String diseases() {
+        return "diseases";
+    }
+}
